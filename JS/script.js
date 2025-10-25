c9b7814821ca8431560867b59eb08446f33104f7
Vinicius Nascimento <50060629+Vnf13@users.noreply.github.com>
2025-10-25 16:55:23 -0300

    Update script.js
    Aumentando a transição da tela

--- JS/script.js
+++ JS/script.js
@@ -25,9 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Transições entre telas (0 → 1 → 2 → 3)
-    setTimeout(nextScreen, 3000);  // 0 → 1
-    setTimeout(nextScreen, 6000);  // 1 → 2
-    setTimeout(nextScreen, 9000);  // 2 → 3
+    setTimeout(nextScreen, 6000);  // 0 → 1
+    setTimeout(nextScreen, 12000);  // 1 → 2
+    setTimeout(nextScreen, 18000);  // 2 → 3
   }, { once: true });
 
   /* === Controle manual da música === */
@@ -46,4 +46,5 @@ document.addEventListener("DOMContentLoaded", () => {
     isPlaying = !isPlaying;
   });
 });
+
 
